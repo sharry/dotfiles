671ec89e9fab1f599efe8eade62f0b96fe55c69d
Youssef Ben Sadik <youssef@bensadik.net>
2024-12-31 21:41:12 +0100

    refactor: move system defaults to system.nx

--- flake.nix
+++ flake.nix
@@ -14,11 +14,12 @@
 		};
 	};
 
-	outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
+	outputs = { self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
 	let
 	configuration = { pkgs, ... }: {
 		environment.systemPackages = with pkgs; [
 			fd
+			nil
 			fzf
 			deno
 			yazi
@@ -42,64 +43,17 @@
 			localHostName = "bensadik";
 		};
 
-		system.stateVersion = 5;
 		nixpkgs.hostPlatform = "aarch64-darwin";
 		security.pam.enableSudoTouchIdAuth = true;     
 		nix.settings.experimental-features = "nix-command flakes";
-      	system.configurationRevision = self.rev or self.dirtyRev or null;
 
-		system.defaults = {
-
-			loginwindow.LoginwindowText = "youssef@bensadik.net";
-
-			finder = {
-				AppleShowAllFiles = true;
-				AppleShowAllExtensions = true;
-			};
-			
-			dock = {
-				autohide = true;
-				persistent-apps = [];
-				orientation = "right";
-				minimize-to-application = true;
-				wvous-br-corner = 1; # Disable buttom right Hot Corner
-			};
-
-			NSGlobalDomain = {
-				KeyRepeat = 2;
-				AppleMetricUnits = 1;
-				AppleInterfaceStyle = "Dark";
-				AppleICUForce24HourTime = true;
-				AppleTemperatureUnit = "Celsius";
-				AppleMeasurementUnits = "Centimeters";
-				NSDocumentSaveNewDocumentsToCloud = false;
-			};
-
-			CustomUserPreferences = {
-				"com.apple.screencapture" = {
-					target = "clipboard";
-				};
-				
-				"org.p0deje.Maccy" = {
-					ignoredApps = [
-						"com.apple.Passwords"
-						"org.keepassxc.keepassxc"
-					];
-					menuIcon = "paperclip";
-					pasteByDefault = true;
-					showFooter = false;
-					showTitle = false;
-					KeyboardShortcuts_popup = ''{"carbonKeyCode":9,"carbonModifiers":768}'';
-					searchVisibility = "duringSearch";
-				};
-			};	
-		};
 	};
 	user = "momo";
 	in
 	{
 		darwinConfigurations."sharry" = nix-darwin.lib.darwinSystem {
 			modules = [
+				./system.nix
 				./brew.nix
 				configuration
 				nix-homebrew.darwinModules.nix-homebrew
@@ -113,7 +67,7 @@
 				}
 
 				home-manager.darwinModules.home-manager
-          		{
+        {
 					users.users.${user}.home = "/Users/${user}";
 					home-manager = {
 						useGlobalPkgs = true;
---
+++ system.nix
@@ -0,0 +1,49 @@
+{ ... }:
+{
+	system.stateVersion = 5;
+    system.defaults = {
+		loginwindow.LoginwindowText = "youssef@bensadik.net";
+
+		finder = {
+			AppleShowAllFiles = true;
+			AppleShowAllExtensions = true;
+		};
+		
+		dock = {
+			autohide = true;
+			persistent-apps = [];
+			orientation = "right";
+			minimize-to-application = true;
+			wvous-br-corner = 1; # Disable buttom right Hot Corner
+		};
+
+		NSGlobalDomain = {
+			KeyRepeat = 2;
+			AppleMetricUnits = 1;
+			AppleInterfaceStyle = "Dark";
+			AppleICUForce24HourTime = true;
+			AppleTemperatureUnit = "Celsius";
+			AppleMeasurementUnits = "Centimeters";
+			NSDocumentSaveNewDocumentsToCloud = false;
+		};
+
+		CustomUserPreferences = {
+			"com.apple.screencapture" = {
+				target = "clipboard";
+			};
+			
+			"org.p0deje.Maccy" = {
+				ignoredApps = [
+					"com.apple.Passwords"
+					"org.keepassxc.keepassxc"
+				];
+				menuIcon = "paperclip";
+				pasteByDefault = true;
+				showFooter = false;
+				showTitle = false;
+				KeyboardShortcuts_popup = ''{"carbonKeyCode":9,"carbonModifiers":768}'';
+				searchVisibility = "duringSearch";
+			};
+		};
+    };
+}
