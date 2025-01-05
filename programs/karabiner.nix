75379323ef59699032e5ff3947220e77e0d518d6
Youssef Ben Sadik <youssef@bensadik.net>
2025-01-05 17:54:33 +0100

    refactor: update karabiner configuration to use builtins.toJSON for improved readability

--- programs/karabiner.nix
+++ programs/karabiner.nix
@@ -3,21 +3,19 @@ let
   configPath = ".config/karabiner/karabiner.json";
 in
 {
-  home.file.${configPath}.text = ''
-  {
-    "profiles": [
-        {
-            "name": "Default profile",
-            "selected": true,
-            "simple_modifications": [
-                {
-                    "from": { "key_code": "caps_lock" },
-                    "to": [{ "key_code": "delete_forward" }]
-                }
-            ],
-            "virtual_hid_keyboard": { "keyboard_type_v2": "ansi" }
-        }
-    ]
-  }
-  '';
+  home.file.${configPath}.text = builtins.toJSON {
+    profiles = [
+      {
+        name = "Default profile";
+        selected = true;
+        simple_modifications = [
+          {
+            from = { key_code = "caps_lock"; };
+            to = [{ key_code = "delete_forward"; }];
+          }
+        ];
+        virtual_hid_keyboard = { keyboard_type_v2 = "ansi"; };
+      }
+    ];
+  };
 }
