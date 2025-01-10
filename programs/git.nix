6aeaebca070a6c0c8566ca32afb762e3eb219f1f
Youssef Ben Sadik <youssef@bensadik.net>
2025-01-10 23:39:23 +0100

    config(git): ignore backup files

--- programs/git.nix
+++ programs/git.nix
@@ -1,14 +1,15 @@
 {
-  programs.git = {
-    enable = true;
-    userName = "Youssef Ben Sadik";
-    userEmail = "youssef@bensadik.net";
-    ignores = [
-      ".DS_Store"
-    ];
-    extraConfig = {
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-    };
-  };
+	programs.git = {
+		enable = true;
+		userName = "Youssef Ben Sadik";
+		userEmail = "youssef@bensadik.net";
+		ignores = [
+			".DS_Store"
+			"*.*~"
+		];
+		extraConfig = {
+			init.defaultBranch = "main";
+			push.autoSetupRemote = true;
+		};
+	};
 }
