c496ba3686a1028af0ffff0d0258aadd5b767c75
Youssef Ben Sadik <youssef@bensadik.net>
2025-02-28 10:45:30 +0000

    config(nvim): bring back `fzf-lua`

--- programs/nvim/lua/config/keymaps.lua
+++ programs/nvim/lua/config/keymaps.lua
@@ -4,9 +4,7 @@ local map = vim.keymap.set
 local telescope = require('telescope.builtin')
 
 map('n', '<leader><space>', "<cmd>Telescope frecency workspace=CWD<cr>", { desc = 'Telescope find files' })
-map('n', '<leader>/', telescope.live_grep, { desc = 'Telescope live grep' })
 map('n', '<leader>b', telescope.buffers, { desc = 'Telescope buffers' })
-map('n', '<leader>ss', telescope.lsp_document_symbols, { desc = 'LSP Document Symbols' })
 
 map("n", "go", "<C-o>", { desc = "Jump back" })
 map("n", "gi", "<C-i>", { desc = "Jump forward" })
--- programs/nvim/lua/plugins/disabled.lua
+++ programs/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
 	{ "akinsho/bufferline.nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "ibhagwan/fzf-lua", enabled = false },
 }
