9ed1fab8e30ec1fe44aaea9c02f3d2eacb828228
Youssef Ben Sadik <youssef@bensadik.net>
2025-03-04 00:28:51 +0000

    config(nvim): remove time from statusline

--- programs/nvim/lua/plugins/statusline.lua
+++ programs/nvim/lua/plugins/statusline.lua
@@ -32,6 +32,48 @@ return {
 						},
 					},
 				},
+				lualine_y = {
+					{
+						function()
+							local expandtab = vim.bo.expandtab
+							local shiftwidth = vim.bo.shiftwidth
+							local tabsize = vim.bo.tabstop
+							if expandtab then
+								return string.format("SPC %d", shiftwidth)
+							else
+								return string.format("TAB %d", tabsize)
+							end
+						end,
+					},
+					{
+						function()
+							local fileformat = vim.bo.fileformat
+
+							local separators = {
+								unix = "LF",
+								dos = "CRLF",
+								mac = "CR"
+							}
+
+							return separators[fileformat] or separators[1]
+						end,
+					},
+					{
+						function ()
+							local encoding = vim.bo.fenc
+							return string.upper(encoding)
+						end,
+					}
+				},
+				lualine_z = {
+					{
+						function()
+							local col = vim.fn.col(".")
+							local line = vim.fn.line(".")
+							return string.format("%d:%d  ", col, line)
+						end,
+					}
+				}
 			},
 		},
 	},
