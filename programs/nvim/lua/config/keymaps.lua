366a0ece3ab723bd421e2bfc233075bf4aec062d
Youssef Ben Sadik <youssef@bensadik.net>
2025-03-06 19:34:18 +0000

    config: auto tab rename
    - (zsh) Auto-rename zj tabs
    - (ghostty) default catppuccin ANSI colors
    - (vim) open lazygit and serpl in zj floting pane

--- programs/nvim/lua/config/keymaps.lua
+++ programs/nvim/lua/config/keymaps.lua
@@ -13,3 +13,21 @@ map("n", "<S-Down>", "10j", { desc = "Scroll Down" })
 map("n", "<S-Left>", "zH", { desc = "Scroll Left" })
 map("n", "<S-Right>", "zL", { desc = "Scroll Right" })
 map("n", "<leader>fx", ":!chmod +x %<CR>", { desc = "Make file executable" })
+
+if vim.fn.executable("lazygit") == 1 then
+	map("n", "<leader>gg",
+		function()
+			vim.cmd("silent !$SHELL -i -c 'g'")
+		end,
+		{ desc = "Lazygit (Root Dir)" }
+	)
+end
+
+if vim.fn.executable("serpl") == 1 then
+	map("n", "<leader>sr",
+		function()
+			vim.cmd("silent !$SHELL -i -c 'sr'")
+		end,
+		{ desc = "Serpl (Search & Replace)" }
+	)
+end
